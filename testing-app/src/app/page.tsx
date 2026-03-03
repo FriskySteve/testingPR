7f90354c6d87372b1b818a1c23f2ede8919454fd
Michal Stasiak <michal.stasiak@protonmail.com>
2026-03-03 19:10:31 +0100

    feat: zmiany chain03

--- testing-app/src/app/page.tsx
+++ testing-app/src/app/page.tsx
@@ -19,6 +19,7 @@ export default function Home() {
           <div>
             <h1>Zmiany chain-01</h1>
             <h1>Zmiany chain-02</h1>
+            <h1>Zmiany chain-03</h1>
           </div>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
