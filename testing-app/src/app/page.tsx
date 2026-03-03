b5e142ee347e7e8e1321e76af1cf5b10363465e0
Michal Stasiak <michal.stasiak@protonmail.com>
2026-03-03 18:56:38 +0100

    feat: zmiany test02

--- testing-app/src/app/page.tsx
+++ testing-app/src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
           </h1>
           <div>
             <h1>Zmiany test-01</h1>
+            <h1>Zmiany test-02</h1>
           </div>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
